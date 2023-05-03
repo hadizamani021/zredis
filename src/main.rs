d0aa6196c6b8021f8d532c9b2d57e25096794358
hadizamani021 <hadiza1231377@gmail.com>
2023-05-03 15:29:08 +0430

    add pong response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*},
+    net::{TcpListener, TcpStream},
+};
+use resp::{encode, Value};
+
+fn handle_pong(mut stream: TcpStream) {
+    let response = Value::String("PONG".to_string());
+    let response = encode(&response);
+    stream.write_all(&response).unwrap();
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -6,8 +16,9 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_pong(stream);
+                println!("handle one request successfully.")
             }
             Err(e) => {
                 println!("error: {}", e);
