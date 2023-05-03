4a824ce5e08082d0384f9742639f856d05445e3f
hadizamani021 <hadiza1231377@gmail.com>
2023-05-03 16:13:51 +0430

    answer to multiple ping in one connection

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 use std::{
     io::{prelude::*},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener},
 };
 
-fn handle_pong(mut stream: TcpStream) {
-    let response = "+PONG\r\n";
-    stream.write(response.as_bytes()).unwrap();
-}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
+                let response = "+PONG\r\n";
                 let mut buf = [0; 512];
-                stream.read(&mut buf).unwrap();
-                handle_pong(stream);
+                loop{                    
+                    let number_of_read_bytes = stream.read(&mut buf).unwrap();
+                    
+                    if number_of_read_bytes == 0{
+                        break;
+                    }
+                    stream.write(response.as_bytes()).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
